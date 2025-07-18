6bff39642be0783ac1da108e4213f94fb356a738
LaerteDelmondes <delmondesoliveiralaerte@gmail.com>
2025-07-18 12:27:10 -0300

    Calculando o valor total

--- js/app.js
+++ js/app.js
@@ -1,11 +1,25 @@
+let totalGeral;
+limpar();
+
 function adicionar() {
 
-    let nomeProduto = document.getElementById('produto');
-    let quantidade = document.getElementById('quantidade');
-    alert(nomeProduto.value);
-    alert(quantidade.value);
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitário = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+    let preco = quantidade * valorUnitário;
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">${preco}</span>
+</section>`;
+    totalGeral = totalGeral + preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `Total: R$ ${totalGeral}`;
+    document.getElementById('quantidade').value = '';
 }
 
 function limpar() {
-
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0';
 }
